cf84a04ac1e0318fe470a1b99663cfa050d8c67f
Gabriel Nützi <gnuetzi@gmail.com>
2025-05-08 14:42:33 +0200

    fix: script

--- examples/what-is-my-ip.nix
+++ examples/what-is-my-ip.nix
@@ -1,9 +1,11 @@
 {
   system ? builtins.currentSystem,
   pkgs ?
-    (import (
-      fetchTarball "https://github.com/NixOS/nixpkgs/archive/9684b53175fc6c09581e94cc85f05ab77464c7e3.tar.gz"
-    )).legacyPackages.${system},
+    import
+      (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/9684b53175fc6c09581e94cc85f05ab77464c7e3.tar.gz")
+      {
+        inherit system;
+      },
 }:
 pkgs.writeShellScriptBin "what-is-my-ip" ''
   ${pkgs.curl}/bin/curl -s http://httpbin.org/get | \
